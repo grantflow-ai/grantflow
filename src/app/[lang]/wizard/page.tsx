f8fadcc7a3cc020d1d35cda4d93fb7c08ab6dee8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-02 12:31:04 +0200

    chore: iteraion 0

--- src/app/[lang]/wizard/page.tsx
+++ src/app/[lang]/wizard/page.tsx
@@ -1,9 +1,43 @@
-import { ActivityCodeSelect } from "@/components/wizard/activity-code-select/activity-code-select";
+import { GrantApplication } from "@/components/wizard/dynamic-forms/section";
+import sectionsData from "@/components/wizard/dynamic-forms/sections.json";
+import questionsData from "@/components/wizard/dynamic-forms/questions.json";
+
+import { z } from "zod";
+
+const QuestionSchema = z.object({
+	questionId: z.number(),
+	questionText: z.string(),
+	required: z.boolean(),
+	allowFileUpload: z.boolean(),
+	dependsOn: z.number().or(z.array(z.number())).nullable(),
+	answerType: z.enum(["text", "boolean", "date-range", "date"]),
+	maxLength: z.number().nullable(),
+});
+
+const SectionSchema = z.object({
+	sectionId: z.number(),
+	name: z.string(),
+	description: z.string(),
+});
+
+const QuestionsSchema = z.record(z.string(), z.array(QuestionSchema));
+const SectionsArraySchema = z.array(SectionSchema);
+
+type Question = z.infer<typeof QuestionSchema>;
+type Section = z.infer<typeof SectionSchema> & { questions: Question[] };
 
 export default function GrantWizard() {
+	const questions = QuestionsSchema.parse(questionsData);
+	const sections = SectionsArraySchema.parse(sectionsData);
+
+	const sectionsWithQuestions: Section[] = sections.map((section) => ({
+		...section,
+		questions: questions[section.name],
+	}));
+
 	return (
 		<section className="flex justify-center">
-			<ActivityCodeSelect />
+			<GrantApplication sections={sectionsWithQuestions as any} />
 		</section>
 	);
 }
