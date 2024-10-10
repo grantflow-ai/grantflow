b4fd155fb3544ea703ab978a0ecdd042ab0153b8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-10 09:49:48 +0200

    chore: updated db queries

--- src/components/wizard/dynamic-forms/dynamic-wizard.tsx
+++ src/components/wizard/dynamic-forms/dynamic-wizard.tsx
@@ -1,13 +1,35 @@
 "use client";
 
-import type { GrantApplicationQuestion, GrantCFP, GrantWizardSection } from "@/types/database-types";
+import {
+	GrantApplicationAnswer,
+	GrantApplicationQuestion,
+	GrantCFP,
+	GrantWizardSection,
+	ResearchAim,
+	ResearchTask,
+} from "@/types/database-types";
+import { getStore } from "@/stores/wizard";
+import { useEffect } from "react";
 
 export function DynamicWizard({
+	draftId,
 	cfp,
 }: {
+	draftId: string;
 	cfp: GrantCFP & {
 		sections: (GrantWizardSection & { questions: GrantApplicationQuestion[] })[];
 	};
+	answers: GrantApplicationAnswer[];
+	researchAims: (ResearchAim & { tasks: ResearchTask[] })[];
 }) {
+	const store = getStore({ cfpIdentifier: cfp.grant_identifier, draftId })();
+
+	useEffect(() => {
+		store.setSections(cfp.sections);
+		// return () => {
+		// 	store.resetStore()
+		// }
+	}, [cfp]);
+
 	return <div></div>;
 }
