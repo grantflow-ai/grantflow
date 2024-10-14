3ce6b042f6b1496819b6ebd8bd440718b7ba3873
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-14 17:09:27 +0200

    chore: fix db seed

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -2,17 +2,13 @@
 
 import { isEmail } from "validator";
 
-import { ApiPath } from "@/enums";
 import { ErrorType } from "@/constants";
-import { urlWithHost } from "@/utils/url";
 
 /**
  * Sign out the current user.
  * @returns The path to redirect to.
  */
 export async function signOut() {
-	const { error } = await supabase.auth.signOut();
-
 	return error?.message;
 }
 
@@ -25,12 +21,6 @@ export async function signInWithEmail(email: string) {
 	if (!isEmail(email)) {
 		return ErrorType.INVALID_EMAIL;
 	}
-
-	const supabase = await getServerClient();
-	const { error } = await supabase.auth.signInWithOtp({
-		email,
-		options: { emailRedirectTo: urlWithHost(ApiPath.CALLBACK_MAGIC_LINK) },
-	});
 
 	return error?.message;
 }
