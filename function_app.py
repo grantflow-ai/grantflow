ec1676536adc69e0d482e92a25bbb794c2f8afa5
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-20 11:34:20 +0200

    chore: update function app setup

--- function_app.py
+++ function_app.py
@@ -5,20 +5,8 @@ This file is the entry point for the Azure Function App.
 
 from azure.functions import FunctionApp
 
-from src.app import blob_trigger_handler
+from src.app import blueprint
 
 app = FunctionApp()
 
-"""
-- see the documentation on Azure Blob trigger name patterns:
-    https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-blob-trigger?tabs=python-v2%2Cisolated-process%2Cnodejs-v4%2Cextensionv5&pivots=programming-language-python#blob-name-patterns
-- see the documentation about Binding Expressions:
-    https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-expressions-patterns
-"""
-app.function_name(name="parser-indexer")(
-    app.blob_trigger(
-        arg_name="blob",
-        path="grant-application-files/{workspace_id}/{parent_id}/{filename}",
-        connection="AzureWebJobsStorage",
-    )(blob_trigger_handler)
-)
+app.register_blueprint(blueprint)
