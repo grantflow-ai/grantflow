120605a3e74e6d35d57b5b1a0f8d4c2c0ac7356f
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-29 07:44:55 +0200

    chore: updated nav

--- src/app/workspaces/page.tsx
+++ src/app/workspaces/page.tsx
@@ -6,6 +6,7 @@ import { eq } from "drizzle-orm";
 import { workspaces, workspaceUsers } from "db/schema";
 import { handleServerError } from "@/utils/server-side";
 import { PagePath } from "@/enums";
+import { Navbar } from "@/components/navbar";
 
 export default async function WorkspacesListPage() {
 	const session = await auth();
@@ -29,15 +30,22 @@ export default async function WorkspacesListPage() {
 		.where(eq(workspaceUsers.userId, session.user.id));
 
 	return (
-		<div className="w-full h-full">
-			<div className="py-4">
-				<CreateWorkspaceModal />
-			</div>
-			<div className="my-6 space-y-8">
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-					{userWorkspaces.map((data) => (
-						<WorkspaceCard key={data.workspaceId} {...data} />
-					))}
+		<div className="flex flex-col flex-1 ml-14">
+			<Navbar>
+				<span className="px-2 text-sm">Workspaces</span>
+			</Navbar>
+			<div className="mt-14 p-4">
+				<div className="w-full h-full">
+					<div className="py-4">
+						<CreateWorkspaceModal />
+					</div>
+					<div className="my-6 space-y-8">
+						<div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+							{userWorkspaces.map((data) => (
+								<WorkspaceCard key={data.workspaceId} {...data} />
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
