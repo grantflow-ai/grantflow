d050f7ef6e3b76a5ed6b93a15d9262ad2b57953d
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-11-11 17:55:24 +0100

    chore: updated db seed

--- scripts/seed-db.ts
+++ scripts/seed-db.ts
@@ -1,40 +1,57 @@
 /* eslint-disable no-console */
-import nihActivityCodes from "./nih-activity-codes.json";
 import { getDatabaseClient } from "db/connection";
-import { fundingOrganizations, grantCfps } from "db/schema";
-
-interface CFPRecord {
-	category: string;
-	code: string;
-	title: string;
-	description: string;
-	url?: string;
-}
+import {
+	accounts,
+	fundingOrganizations,
+	grantCfps,
+	users,
+	workspaces,
+	workspaceUsers,
+	grantApplications,
+	researchSignificances,
+	researchInnovations,
+	researchTasks,
+	researchAims,
+} from "db/schema";
+import userRecords from "./users.json";
+import accountRecords from "./accounts.json";
+import fundingOrganizationRecords from "./funding_organizations.json";
+import grantCFPRecords from "./grant_cfps.json";
+import workspaceRecords from "./workspaces.json";
+import grantApplicationsRecords from "./grant_applications.json";
+import workspaceUserRecords from "./workspace_users.json";
+import researchSignificancesRecords from "./research_significances.json";
+import researchInnovationsRecords from "./research_innovations.json";
+import researchAimsRecords from "./research_aims.json";
+import researchTasksRecords from "./research_tasks.json";
+import {
+	FundingOrganization,
+	GrantApplication,
+	GrantCFP,
+	ResearchAim,
+	ResearchInnovation,
+	ResearchSignificance,
+	ResearchTask,
+	User,
+	Workspace,
+} from "@/types/database-types";
 
 async function seedDatabase() {
 	const db = getDatabaseClient();
 
-	const [{ fundingOrganizationId }] = await db
-		.insert(fundingOrganizations)
-		.values({
-			name: "NIH",
-		})
-		.returning({ fundingOrganizationId: fundingOrganizations.id });
-
-	console.log("Funding organization inserted");
-
-	const results = await db
-		.insert(grantCfps)
-		.values(
-			(nihActivityCodes as CFPRecord[]).map(({ url, ...cfp }) => ({
-				...cfp,
-				url: url ?? null,
-				fundingOrganizationId,
-			})),
-		)
-		.returning({ id: fundingOrganizations.id });
-
-	console.log(`Inserted ${results.length} records`);
+	console.log("Seeding database...");
+	await db.insert(users).values(userRecords as unknown as User[]);
+	await db.insert(accounts).values(accountRecords as never[]);
+	await db.insert(fundingOrganizations).values(fundingOrganizationRecords as unknown as FundingOrganization[]);
+	await db.insert(grantCfps).values(grantCFPRecords as unknown as GrantCFP[]);
+	await db.insert(workspaces).values(workspaceRecords as unknown as Workspace[]);
+	await db.insert(workspaceUsers).values(workspaceUserRecords as never[]);
+	await db.insert(grantApplications).values(grantApplicationsRecords as unknown as GrantApplication[]);
+	await db.insert(researchSignificances).values(researchSignificancesRecords as unknown as ResearchSignificance[]);
+	await db.insert(researchInnovations).values(researchInnovationsRecords as unknown as ResearchInnovation[]);
+	await db.insert(researchAims).values(researchAimsRecords as unknown as ResearchAim[]);
+	await db.insert(researchTasks).values(researchTasksRecords as unknown as ResearchTask[]);
+	console.log("Database seeded successfully");
 }
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
