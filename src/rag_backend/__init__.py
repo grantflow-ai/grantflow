7460e23a3dfea39e7d73794d887418c0c6b6eb06
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-11-16 16:31:30 +0100

    chore: reverted to python 3.11

--- src/rag_backend/__init__.py
+++ src/rag_backend/__init__.py
@@ -4,7 +4,7 @@ from azure.functions import Blueprint
 
 from .handlers import handle_section_generation_request
 
-blueprint = Blueprint(name="rag_backend-api")  # type: ignore[no-untyped-call]
+blueprint = Blueprint(name="rag-api")  # type: ignore[no-untyped-call]
 
 blueprint.function_name(name=handle_section_generation_request.__name__)(
     blueprint.route(route="generate-section-text", methods=[HTTPMethod.POST])(
