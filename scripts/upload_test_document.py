9c037df922f449a953699a117ad7e535256b21c6
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-11-25 14:47:09 +0100

    chore: updated file uploads

--- scripts/upload_test_document.py
+++ scripts/upload_test_document.py
@@ -10,25 +10,40 @@ logger = logging.getLogger(__name__)
 
 WORKSPACE_ID = "3d8f40f3-5053-4b55-b431-ede2a167201c"
 APPLICATION_ID = "9e4cf818-a3f6-4f3e-9cc2-6e8a3fcf3a05"
-SECTION_NAME = "research-plan"
+
+INNOVATION_FILES = [
+    "EIC_SIGNIFICANCE.docx",
+    "EIC_INNOVATION.docx",
+]
+
+RESEARCH_PLAN_FILES = ["EIC_AIMS.docx", "EIC_PRELIM.docx", "EIC_RISKS.docx"]
 
 
-async def upload_test_document() -> None:
+async def upload_test_documents() -> None:
     """Upload a test document to the blob storage."""
     load_settings_and_set_env(logger)
 
     blob_service_client = BlobServiceClient.from_connection_string(get_env("AzureWebJobsStorage"))
     container_client = blob_service_client.get_container_client("grant-application-files")
 
-    test_file = Path(__file__).parent / "test_application.pdf"
-    data = test_file.read_bytes()
+    for file in INNOVATION_FILES:
+        test_file = Path(__file__).parent / file
+        data = test_file.read_bytes()
 
-    filename = f"{WORKSPACE_ID}/{APPLICATION_ID}/{SECTION_NAME}/test_application.pdf"
-    client = await container_client.upload_blob(name=filename, data=data, overwrite=True)
-    await client.close()  # type: ignore[no-untyped-call]
+        filename = f"{WORKSPACE_ID}/{APPLICATION_ID}/significance-and-innovation/{file}"
+        await container_client.upload_blob(name=filename, data=data, overwrite=True)
+
+    for file in RESEARCH_PLAN_FILES:
+        test_file = Path(__file__).parent / file
+        data = test_file.read_bytes()
+
+        filename = f"{WORKSPACE_ID}/{APPLICATION_ID}/research-plan/{file}"
+        await container_client.upload_blob(name=filename, data=data, overwrite=True)
+
+    await container_client.close()  # type: ignore[no-untyped-call]
 
 
 if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(upload_test_document())
+    asyncio.run(upload_test_documents())
