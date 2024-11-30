116e0e0d12d62944f3fe8d28d15aec0a0628fc4d
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-11-30 10:18:15 +0100

    chore: reverted to using azure document intelligence

--- src/indexer/extraction.py
+++ src/indexer/extraction.py
@@ -2,25 +2,29 @@ from __future__ import annotations
 
 import logging
 from mimetypes import guess_type
-from tempfile import NamedTemporaryFile
-from typing import Final, cast
+from typing import Any, Final, NotRequired, TypedDict, cast
 
+from azure.ai.documentintelligence.aio import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest, ContentFormat
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
 from charset_normalizer import detect
-from pymupdf4llm.helpers.pymupdf_rag import to_markdown
 from pypandoc import convert_text
 
-from src.utils.exceptions import ValidationError
+from src.utils.env import get_env
+from src.utils.exceptions import FileParsingError, ValidationError
 from src.utils.sync import as_async_callable
 
 logger = logging.getLogger(__name__)
 
 MARKDOWN_MIME_TYPE: Final[str] = "text/markdown"
-PDF_MIMETYPE: Final[str] = "application/pdf"
-PLAIN_TEXT_MIMETYPES: set[str] = {
+
+PLAIN_TEXT_MIME_TYPES: set[str] = {
     MARKDOWN_MIME_TYPE,
     "text/plain",
 }
-PANDOC_MIMETYPES = {
+
+PANDOC_MIME_TYPES = {
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "text/csv",
     "application/csv",
@@ -39,6 +43,19 @@ PANDOC_MIMETYPES = {
     "text/x-latex",
     "application/latex",
     "text/latex",
+}
+
+DOCUMENT_INTELLIGENCE_SUPPORTED_MIME_TYPES: set[str] = {
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "image/bmp",
+    "image/gif",
+    "image/heif",
+    "image/jpeg",
+    "image/png",
+    "image/tiff",
 }
 
 MIME_TYPE_EXT_MAP = {
@@ -63,7 +80,144 @@ MIME_TYPE_EXT_MAP = {
 }
 
 pandoc_handler = as_async_callable(convert_text)
-pdf_handler = as_async_callable(to_markdown)
+
+
+class Span(TypedDict, total=False):
+    """A span of text in a document."""
+
+    offset: NotRequired[int]
+    length: NotRequired[int]
+
+
+class BoundingRegion(TypedDict, total=False):
+    """A bounding region in a document."""
+
+    pageNumber: NotRequired[int]
+    polygon: NotRequired[list[float]]
+
+
+class Word(TypedDict, total=False):
+    """A word in a document."""
+
+    content: str
+    confidence: NotRequired[float]
+    span: NotRequired[Span]
+    polygon: NotRequired[list[float]]
+
+
+class SelectionMark(TypedDict, total=False):
+    """A selection mark in a document."""
+
+    state: str
+    confidence: NotRequired[float]
+    polygon: NotRequired[list[float]]
+    span: NotRequired[Span]
+
+
+class Line(TypedDict, total=False):
+    """A line in a document."""
+
+    content: str
+    polygon: NotRequired[list[float]]
+    spans: NotRequired[list[Span]]
+
+
+class Page(TypedDict, total=False):
+    """A page in a document."""
+
+    pageNumber: NotRequired[int]
+    words: NotRequired[list[Word]]
+    spans: NotRequired[list[Span]]
+    angle: NotRequired[float]
+    width: NotRequired[float]
+    height: NotRequired[float]
+    unit: NotRequired[str]
+    selectionMarks: NotRequired[list[SelectionMark]]
+    lines: NotRequired[list[Line]]
+
+
+class TableCell(TypedDict, total=False):
+    """A table cell in a document."""
+
+    rowIndex: NotRequired[int]
+    columnIndex: NotRequired[int]
+    content: str
+    boundingRegions: NotRequired[list[BoundingRegion]]
+    spans: NotRequired[list[Span]]
+    elements: NotRequired[list[str]]
+    columnSpan: NotRequired[int]
+    rowSpan: NotRequired[int]
+    kind: NotRequired[str]
+
+
+class Table(TypedDict, total=False):
+    """A table in a document."""
+
+    rowCount: NotRequired[int]
+    columnCount: NotRequired[int]
+    cells: NotRequired[list[TableCell]]
+    boundingRegions: NotRequired[list[BoundingRegion]]
+    spans: NotRequired[list[Span]]
+
+
+class Paragraph(TypedDict, total=False):
+    """A paragraph in a document."""
+
+    spans: NotRequired[list[Span]]
+    boundingRegions: NotRequired[list[BoundingRegion]]
+    content: str
+    role: NotRequired[str]
+
+
+class Style(TypedDict, total=False):
+    """A style in a document."""
+
+    confidence: NotRequired[float]
+    spans: NotRequired[list[Span]]
+    isHandwritten: NotRequired[bool]
+
+
+class Section(TypedDict, total=False):
+    """A section in a document."""
+
+    spans: NotRequired[list[Span]]
+    elements: NotRequired[list[str]]
+
+
+class FigureCaption(TypedDict, total=False):
+    """A figure caption in a document."""
+
+    content: str
+    boundingRegions: NotRequired[list[BoundingRegion]]
+    spans: NotRequired[list[Span]]
+    elements: NotRequired[list[str]]
+
+
+class Figure(TypedDict, total=False):
+    """A figure in a document."""
+
+    id: str
+    boundingRegions: NotRequired[list[BoundingRegion]]
+    spans: NotRequired[list[Span]]
+    elements: NotRequired[list[str]]
+    caption: NotRequired[FigureCaption]
+
+
+class OCROutput(TypedDict, total=False):
+    """The raw output from the Azure Document Intelligence prebuilt-layout model api."""
+
+    apiVersion: str
+    modelId: str
+    stringIndexType: str
+    content: str
+    pages: NotRequired[list[Page]]
+    tables: NotRequired[list[Table]]
+    paragraphs: NotRequired[list[Paragraph]]
+    styles: NotRequired[list[Style]]
+    contentFormat: NotRequired[str]
+    sections: NotRequired[list[Section]]
+    figures: NotRequired[list[Figure]]
+    additionalItems: NotRequired[list[str | dict[str, Any]]]
 
 
 async def extract_with_pandoc(file_data: bytes, mime_type: str) -> str:
@@ -82,26 +236,45 @@ async def extract_with_pandoc(file_data: bytes, mime_type: str) -> str:
     return cast(str, await pandoc_handler(file_data, to="md", format=ext, encoding=encoding))
 
 
-async def extract_pdf(file_data: bytes) -> str:
-    """Extract text from a PDF file.
+async def extract_with_azure_document_intelligence(file_content: bytes) -> OCROutput:
+    """Extract text from a document using the Azure Document Intelligence prebuilt-layout model.
 
     Args:
-        file_data: The content of the file.
+        file_content: The content of the document.
+
+    Raises:
+        FileParsingError: If an error occurs during the extraction.
 
     Returns:
-        The extracted text.
+        The extracted text from the document.
     """
-    with NamedTemporaryFile(suffix=".pdf", delete=False, mode="wb+") as temp_file:
-        temp_file.write(file_data)
-        temp_file.flush()
-        return cast(str, await pdf_handler(temp_file.name, show_progress=False))
+    client = DocumentIntelligenceClient(
+        endpoint=get_env("AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT"),
+        credential=AzureKeyCredential(get_env("AZURE_DOCUMENT_INTELLIGENCE_KEY")),
+    )
+    try:
+        poller = await client.begin_analyze_document(
+            model_id="prebuilt-layout",
+            analyze_request=AnalyzeDocumentRequest(bytes_source=file_content),
+            output_content_format=ContentFormat.MARKDOWN,
+        )
+        result = await poller.result()
+        return cast(OCROutput, result.as_dict())
+    except HttpResponseError as e:
+        logger.error("Error extracting text from from file: %s", e)
+        raise FileParsingError(
+            "Error extracting text from file",
+            context=str(e),
+        ) from e
+    finally:
+        await client.close()
 
 
 async def parse_file_data(
     *,
     file_data: bytes,
     filename: str,
-) -> tuple[str, str]:
+) -> tuple[str | OCROutput, str]:
     """Extract the contents of a file.
 
     Args:
@@ -115,13 +288,15 @@ async def parse_file_data(
         A tuple composed of the extracted text and mime_type.
     """
     if mime_type := guess_type(filename)[0]:
-        if mime_type in PLAIN_TEXT_MIMETYPES or any(mime_type.startswith(value) for value in PLAIN_TEXT_MIMETYPES):
+        if mime_type in PLAIN_TEXT_MIME_TYPES or any(mime_type.startswith(value) for value in PLAIN_TEXT_MIME_TYPES):
             return file_data.decode(), mime_type
 
-        if mime_type in PANDOC_MIMETYPES:
+        if mime_type in PANDOC_MIME_TYPES or any(mime_type.startswith(value) for value in PANDOC_MIME_TYPES):
             return await extract_with_pandoc(file_data, mime_type), MARKDOWN_MIME_TYPE
 
-        if mime_type == PDF_MIMETYPE:
-            return await extract_pdf(file_data), MARKDOWN_MIME_TYPE
+        if mime_type in DOCUMENT_INTELLIGENCE_SUPPORTED_MIME_TYPES or any(
+            mime_type.startswith(value) for value in DOCUMENT_INTELLIGENCE_SUPPORTED_MIME_TYPES
+        ):
+            return await extract_with_azure_document_intelligence(file_data), MARKDOWN_MIME_TYPE
 
     raise ValidationError(f"Unsupported mime type for file extraction: {mime_type}")
