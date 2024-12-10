49ea5e77a379566032413ef0dba6f5570a78d50e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-10 14:40:29 +0100

    chore: cleanup

--- src/exceptions.ts
+++
@@ -1,9 +0,0 @@
-export class NotAuthenticatedError extends Error {
-	constructor(message = 'User is not authenticated') {
-		super(message);
-		this.name = 'NotAuthenticatedError';
-
-		// Maintains proper stack trace for where error was thrown
-		Object.setPrototypeOf(this, NotAuthenticatedError.prototype);
-	}
-}
