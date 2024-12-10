5e5997d58f85588266e9cfb71dfd4719b3c15bc6
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-10 16:54:22 +0100

    chore: add websocket

---
+++ src/utils/time.ts
@@ -0,0 +1,14 @@
+/**
+ * The wait function returns a promise that resolves after the specified timeout.
+ *
+ * @param timeout number Set the amount of time to wait before resolving
+ *
+ * @returns a void promise;
+ */
+export function wait(timeout: number): Promise<void> {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, timeout);
+	});
+}
