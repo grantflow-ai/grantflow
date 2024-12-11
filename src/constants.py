cf96de272d870eaf512310b20cccb20efb4da929
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-11 19:34:01 +0100

    chore: add health check

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 from typing import Final
 
 CONTENT_TYPE_JSON: Final[str] = "application/json"
+CONTENT_TYPE_TEXT: Final[str] = "text/plain"
 ONE_MINUTE_SECONDS: Final[int] = 60
 
 # Model constants
