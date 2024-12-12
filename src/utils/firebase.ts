1fad59b5e47fe1e752879991f90599482f526ab7
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-12 11:35:46 +0100

    chore: updated env

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -17,7 +17,12 @@ export function getFirebaseApp(): FirebaseApp {
 	if (!instanceRef.app) {
 		instanceRef.app = initializeApp({
 			apiKey: getEnv().NEXT_PUBLIC_FIREBASE_API_KEY,
+			appId: getEnv().NEXT_PUBLIC_FIREBASE_APP_ID,
 			authDomain: getEnv().NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+			measurementId: getEnv().NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+			messagingSenderId: getEnv().NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+			projectId: getEnv().NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+			storageBucket: getEnv().NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 		});
 	}
 
