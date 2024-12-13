94f5ec3ca769518dcc219c27b50a87666ebe32d7
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-13 18:39:33 +0100

    chore: updated server action max body size

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,14 @@ import { createJiti } from "jiti";
 const jiti = createJiti(import.meta.url);
 const { getEnv } = await jiti.import("./src/utils/env.ts");
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 getEnv();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
 		serverActions: {
-			bodySizeLimit: "20mb",
+			bodySizeLimit: "100mb",
 			timeout: 600,
 		},
 	},
