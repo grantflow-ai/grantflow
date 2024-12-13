9104cc70026090ece65bb169b045fa54a05d7792
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-13 16:05:53 +0100

    chore: updated file handling

--- src/indexer/indexing.py
+++ src/indexer/indexing.py
@@ -1,9 +1,9 @@
 from asyncio import gather
 from typing import Final
 
+from src.dto import VectorDTO
 from src.indexer.chunking import logger
-from src.indexer.db import upsert_application_vectors
-from src.indexer.dto import Chunk, VectorDTO
+from src.indexer.dto import Chunk
 from src.utils.embeddings import TaskType, generate_embeddings
 
 CHUNKS_BATCH_SIZE: Final[int] = 30
@@ -12,13 +12,11 @@ CHUNKS_BATCH_SIZE: Final[int] = 30
 async def create_vector_dto(
     *,
     chunk: Chunk,
-    file_id: str,
 ) -> VectorDTO:
     """Process a single chunked element.
 
     Args:
         chunk: The chunked element.
-        file_id: The ID of the file from which the chunk is derived.
 
     Returns:
         VectorDTO
@@ -35,7 +33,6 @@ async def create_vector_dto(
         content=chunk["content"],
         element_type=chunk["element_type"],
         embedding=embedding,
-        file_id=file_id,
         page_number=chunk["page_number"],
     )
 
@@ -43,15 +40,11 @@ async def create_vector_dto(
 async def index_documents(
     *,
     chunks: list[Chunk],
-    file_id: str,
-    application_id: str,
-) -> None:
+) -> list[VectorDTO]:
     """Create embeddings for the given chunks.
 
     Args:
         chunks: The list of chunks to index.
-        file_id: The ID of the file from which the chunks are derived.
-        application_id: The ID of the application the chunks belong to.
 
     Returns:
         The list of documents to index.
@@ -62,12 +55,10 @@ async def index_documents(
             *[
                 create_vector_dto(
                     chunk=chunk,
-                    file_id=file_id,
                 )
                 for chunk in chunks[i : i + CHUNKS_BATCH_SIZE]
             ]
         )
         data.extend([result for result in results if result is not None])
 
-    await upsert_application_vectors(vectors=data, application_id=application_id)
-    logger.info("Successfully indexed file_id: %s", file_id)
+    return data
--- tests/indexer/indexing_test.py
+++ tests/indexer/indexing_test.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock
 
-from src.indexer.dto import Chunk, VectorDTO
+from src.dto import VectorDTO
+from src.indexer.dto import Chunk
 from src.indexer.indexing import create_vector_dto
 
 
@@ -11,15 +12,13 @@ async def test_create_vector_dto(mock_text_embedding_model: Mock) -> None:
         "page_number": 1,
         "element_type": "paragraph",
     }
-    file_id = "test_file_id"
 
-    vector_dto = await create_vector_dto(chunk=chunk, file_id=file_id)
+    vector_dto = await create_vector_dto(chunk=chunk)
 
     assert vector_dto == VectorDTO(
         chunk_index=0,
         content="test content",
         element_type="paragraph",
         embedding=[1.0, 2.0, 3.0],
-        file_id="test_file_id",
         page_number=1,
     )
