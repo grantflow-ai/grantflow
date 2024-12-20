7105c382924b07a4b5be4931cc3588c37c0479e1
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-12-20 13:28:34 +0100

    chore: added risks and alternatives

--- src/rag/application_draft_generation/research_plan.py
+++ src/rag/application_draft_generation/research_plan.py
@@ -13,6 +13,8 @@ from src.rag.application_draft_generation.research_plan_relations import set_rel
 from src.rag.application_draft_generation.research_tasks import (
     handle_research_task_text_generation,
 )
+from src.rag.application_draft_generation.risks_and_alternatives import handle_risks_and_alternatives_text_generation
+from src.rag.dto import ResearchAimDTO
 from src.utils.logging import get_logger
 
 logger = get_logger(__name__)
@@ -37,7 +39,75 @@ ${preliminary_results_text}
 ##### Research Tasks
 
 ${research_tasks_texts}
+
+##### Risks and Alternatives
+
+${risks_and_alternatives_text}
 """)
+
+
+async def handle_research_aim_components_generation(
+    *,
+    application_id: str,
+    research_aim_dto: ResearchAimDTO,
+    session_maker: async_sessionmaker[Any],
+) -> str:
+    """Generate the text for the research aim and its components.
+
+    Args:
+        application_id: GrantApplication,
+        research_aim_dto: The research aim to generate text for.
+        session_maker: The session maker.
+
+    Returns:
+        The generated research aim text.
+    """
+    logger.info("Generated research aim", application_id=application_id, research_aim_id=research_aim_dto.id)
+
+    research_aim_description_text = await handle_research_aim_description_generation(
+        application_id=application_id,
+        session_maker=session_maker,
+        research_aim_dto=research_aim_dto,
+    )
+
+    research_tasks_texts = await gather(
+        *[
+            handle_research_task_text_generation(
+                session_maker=session_maker,
+                application_id=application_id,
+                requires_clinical_trials=research_aim_dto.requires_clinical_trials,
+                research_task_id=research_task_dto.id,
+                research_task_dto=research_task_dto,
+            )
+            for research_task_dto in sorted(research_aim_dto.research_tasks, key=lambda x: x.task_number)
+        ]
+    )
+
+    preliminary_results_text, risks_and_alternatives_text = tuple(
+        await gather(
+            *[
+                handle_preliminary_results_text_generation(
+                    application_id=application_id,
+                    research_aim_dto=research_aim_dto,
+                    research_aim_description=research_aim_description_text,
+                    session_maker=session_maker,
+                ),
+                handle_risks_and_alternatives_text_generation(
+                    application_id=application_id,
+                    research_aim_dto=research_aim_dto,
+                    research_aim_description=research_aim_description_text,
+                    session_maker=session_maker,
+                ),
+            ]
+        )
+    )
+
+    return RESEARCH_AIM_TEMPLATE.substitute(
+        research_aim_description_text=research_aim_description_text,
+        preliminary_results_text=preliminary_results_text,
+        research_tasks_texts="\n\n".join(research_tasks_texts),
+        risks_and_alternatives_text=risks_and_alternatives_text,
+    ).strip()
 
 
 async def handle_research_plan_text_generation(
@@ -59,44 +129,14 @@ async def handle_research_plan_text_generation(
     logger.info("Entering research plan generation phase", application_id=application_id)
     research_aim_dtos = await set_relation_data(research_aims)
 
-    research_aim_texts: list[str] = []
-
-    for research_aim_dto in research_aim_dtos:
-        logger.info("Generated research aim", application_id=application_id, research_aim_id=research_aim_dto.id)
-
-        research_aim_description_text = await handle_research_aim_description_generation(
-            application_id=application_id,
-            session_maker=session_maker,
-            research_aim_dto=research_aim_dto,
-        )
-
-        preliminary_results_text = await handle_preliminary_results_text_generation(
+    research_aim_texts: list[str] = [
+        await handle_research_aim_components_generation(
             application_id=application_id,
             research_aim_dto=research_aim_dto,
-            research_aim_description=research_aim_description_text,
             session_maker=session_maker,
         )
-
-        research_tasks_texts = await gather(
-            *[
-                handle_research_task_text_generation(
-                    session_maker=session_maker,
-                    application_id=application_id,
-                    requires_clinical_trials=research_aim_dto.requires_clinical_trials,
-                    research_task_id=research_task_dto.id,
-                    research_task_dto=research_task_dto,
-                )
-                for research_task_dto in sorted(research_aim_dto.research_tasks, key=lambda x: x.task_number)
-            ]
-        )
-
-        research_aim_texts.append(
-            RESEARCH_AIM_TEMPLATE.substitute(
-                research_aim_description_text=research_aim_description_text,
-                preliminary_results_text=preliminary_results_text,
-                research_tasks_texts="\n\n".join(research_tasks_texts),
-            ).strip()
-        )
+        for research_aim_dto in sorted(research_aim_dtos, key=lambda x: x.aim_number)
+    ]
 
     logger.info("Generated research aims and tasks", application_id=application_id)
 
