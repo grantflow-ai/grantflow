a70a921437e76490fd1715dbf1365052a9eaceda
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-01-07 19:36:20 +0100

    chore: updated Base.to_dict to handle relationships

--- src/db/base.py
+++ src/db/base.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 
 from sqlalchemy import UUID, DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, class_mapper, mapped_column
+from sqlalchemy.orm.exc import DetachedInstanceError
 from sqlalchemy.sql.functions import now
 
 
@@ -15,13 +16,24 @@ class Base(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=now(), onupdate=now())
 
+    def _get_relationship_value(self, key: str) -> Any:
+        try:
+            return getattr(self, key)
+        except DetachedInstanceError:
+            return None
+
     def to_dict(self) -> dict[str, Any]:
         """Serialize the model's columns to a dictionary.
 
         Returns:
             A dictionary containing the model's columns.
         """
-        return {column.key: getattr(self, column.key) for column in class_mapper(self.__class__).columns}
+        mapper = class_mapper(self.__class__)
+        column_values = {column.key: getattr(self, column.key) for column in mapper.columns}
+        relationship_values = {
+            relationship.key: self._get_relationship_value(relationship.key) for relationship in mapper.relationships
+        }
+        return {**column_values, **relationship_values}
 
 
 class BaseWithUUIDPK(Base):
