302c7fdb189d586c8663c1db797447a0451fe191
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-01-09 16:02:56 +0100

    chore: fixed tests

--- tests/api/routes/applications_test.py
+++ tests/api/routes/applications_test.py
@@ -1,6 +1,7 @@
 from datetime import UTC, datetime
 from http import HTTPStatus
-from typing import Any
+from typing import Any, Final
+from unittest.mock import AsyncMock
 
 from sanic_testing.testing import SanicASGITestClient
 from sqlalchemy import insert, select, update
@@ -11,21 +12,28 @@ from src.api_types import (
     ApplicationDraftProcessingResponse,
     UpdateApplicationRequestBody,
 )
+from src.constants import TEMPLATE_VARIABLE_PATTERN
 from src.db.enums import UserRoleEnum
+from src.db.helpers import retrieve_application
 from src.db.tables import (
     GrantApplication,
+    GrantTemplate,
     Workspace,
     WorkspaceUser,
 )
 from src.utils.serialization import deserialize, serialize
-from tests.factories import CreateApplicationRequestBodyFactory
+from tests.factories import CreateApplicationRequestBodyFactory, TextGenerationResultFactory
+
+TEST_CFP_URL: Final[str] = "https://grants.nih.gov/grants/guide/rfa-files/RFA-DC-25-005.html"
 
 
-async def test_create_application_api_request_success(
+async def test_create_application(
     asgi_client: SanicASGITestClient,
     async_session_maker: async_sessionmaker[Any],
     firebase_uid: str,
     workspace: Workspace,
+    signal_dispatch_mock: AsyncMock,
+    mock_extract_webpage_content: AsyncMock,
 ) -> None:
     async with async_session_maker() as session, session.begin():
         await session.execute(
@@ -34,31 +42,30 @@ async def test_create_application_api_request_success(
             )
         )
 
-    # data = CreateApplicationRequestBodyFactory.build(workspace_id=str(workspace.id))
-
-
-async def test_create_application_no_files_success(
-    asgi_client: SanicASGITestClient,
-    async_session_maker: async_sessionmaker[Any],
-    firebase_uid: str,
-    workspace: Workspace,
-) -> None:
-    async with async_session_maker() as session, session.begin():
-        await session.execute(
-            insert(WorkspaceUser).values(
-                {"workspace_id": workspace.id, "firebase_uid": firebase_uid, "role": UserRoleEnum.MEMBER.value}
-            )
-        )
-
-    data = CreateApplicationRequestBodyFactory.build(workspace_id=str(workspace.id))
+    request_body = CreateApplicationRequestBodyFactory.build(cfp_url=TEST_CFP_URL)
 
     _, response = await asgi_client.post(
         f"/workspaces/{workspace.id}/applications",
-        data={"data": serialize(data).decode()},
+        data={
+            "data": serialize(request_body).decode(),
+        },
         headers={"Authorization": "Bearer some_token"},
     )
+    assert response.status_code == HTTPStatus.CREATED, response.text
 
-    assert response.status_code == HTTPStatus.CREATED
+    response_body = response.json
+    assert response_body["id"]
+
+    grant_application = await retrieve_application(
+        session_maker=async_session_maker, application_id=response_body["id"]
+    )
+
+    assert grant_application.title == request_body["title"]
+
+    signal_calls = [
+        call for call in signal_dispatch_mock.mock_calls if call.args[0] == "handle_generate_grant_template"
+    ]
+    assert len(signal_calls) == 1
 
 
 async def test_retrieve_application_text_processing(
@@ -91,7 +98,15 @@ async def test_retrieve_application_text_complete(
     firebase_uid: str,
     workspace: Workspace,
     grant_application: GrantApplication,
+    grant_template: GrantTemplate,
 ) -> None:
+    generation_results = [
+        TextGenerationResultFactory.build(
+            type=section_type,
+        )
+        for section_type in TEMPLATE_VARIABLE_PATTERN.findall(grant_template.template)
+    ]
+
     async with async_session_maker() as session, session.begin():
         await session.execute(
             insert(WorkspaceUser).values(
@@ -101,8 +116,9 @@ async def test_retrieve_application_text_complete(
         await session.execute(
             update(GrantApplication)
             .where(GrantApplication.id == grant_application.id)
-            .values({"text": "Generated text", "completed_at": datetime.now(tz=UTC)})
+            .values({"completed_at": datetime.now(tz=UTC), "text_generation_results": generation_results})
         )
+        await session.commit()
 
     _, response = await asgi_client.get(
         f"/workspaces/{workspace.id}/applications/{grant_application.id}/content",
@@ -111,8 +127,15 @@ async def test_retrieve_application_text_complete(
 
     assert response.status_code == HTTPStatus.OK
     response_body = deserialize(response.body, ApplicationDraftCompleteResponse)
+
+    expected_result = grant_template.template
+    for generation_result in generation_results:
+        expected_result = expected_result.replace(generation_result["type"], generation_result["content"])
+
+    expected_result = expected_result.replace("{", "").replace("}", "")
+
     assert response_body["status"] == "complete"
-    assert response_body["text"] == "Generated text"
+    assert response_body["text"] == expected_result
 
 
 async def test_update_application_success(
@@ -137,7 +160,7 @@ async def test_update_application_success(
         headers={"Authorization": "Bearer some_token"},
     )
 
-    assert response.status_code == HTTPStatus.NO_CONTENT
+    assert response.status_code == HTTPStatus.OK
 
     async with async_session_maker() as session:
         updated = await session.scalar(select(GrantApplication).where(GrantApplication.id == grant_application.id))
@@ -158,6 +181,29 @@ async def test_update_application_unauthorized(
     )
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+async def test_update_application_bad_request(
+    asgi_client: SanicASGITestClient,
+    async_session_maker: async_sessionmaker[Any],
+    firebase_uid: str,
+    workspace: Workspace,
+    grant_application: GrantApplication,
+) -> None:
+    async with async_session_maker() as session, session.begin():
+        await session.execute(
+            insert(WorkspaceUser).values(
+                {"workspace_id": workspace.id, "firebase_uid": firebase_uid, "role": UserRoleEnum.MEMBER.value}
+            )
+        )
+
+    _, response = await asgi_client.patch(
+        f"/workspaces/{workspace.id}/applications/{grant_application.id}",
+        json={},
+        headers={"Authorization": "Bearer some_token"},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 async def test_delete_application_success(
