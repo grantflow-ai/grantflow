3370d6c9fa98221728312d5cb27838036a2e4488
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-01-20 16:38:25 +0100

    feat: synced api types

--- src/types/api-types.ts
+++ src/types/api-types.ts
@@ -1,86 +1,95 @@
-import { UserRole } from "@/constants";
+export enum FileIndexingStatusEnum {
+	FAILED = "FAILED",
+	FINISHED = "FINISHED",
+	INDEXING = "INDEXING",
+}
+
+export enum UserRoleEnum {
+	ADMIN = "ADMIN",
+	MEMBER = "MEMBER",
+	OWNER = "OWNER",
+}
 
 export interface APIError {
-	details: string;
+	details?: string;
 	message: string;
 }
 
-export interface Application extends ApplicationBase {
-	files: ApplicationFile[];
-	innovation: null | string;
-	research_aims: ResearchAim[];
-	significance: null | string;
+export interface ApplicationDetails {
+	background_context?: string;
+	hypothesis?: string;
+	impact?: string;
+	milestones_and_timeline?: string;
+	novelty_and_innovation?: string;
+	preliminary_data?: string;
+	rationale?: string;
+	research_feasibility?: string;
+	risks_and_mitigations?: string;
+	scientific_infrastructure?: string;
+	team_excellence?: string;
 }
 
-export interface ApplicationBase extends ApplicationId {
-	cfp: GrantCfp;
-	text: null | string;
-	title: string;
-}
-
-export type ApplicationDraftResponse =
-	| {
-			id: string;
-			status: "complete";
-			text: string;
-	  }
-	| {
-			id: string;
-			status: "generating";
-	  };
-
-export interface ApplicationFile {
+export interface ApplicationDraftCompleteResponse {
 	id: string;
-	name: string;
-	size: number;
-	type: string;
+	status: "complete";
+	text: string;
 }
 
-export interface ApplicationId {
+export interface ApplicationDraftProcessingResponse {
 	id: string;
+	status: "generating";
 }
+
+export type ApplicationDraftResponse = ApplicationDraftCompleteResponse | ApplicationDraftProcessingResponse;
 
 export interface CreateApplicationRequestBody {
-	cfp_id: string;
-	innovation: null | string;
-	research_aims: CreateResearchAimRequestBody[];
-	significance: null | string;
+	cfp_url?: string;
 	title: string;
 }
 
-export interface CreateResearchAimRequestBody {
-	aim_number: number;
-	description: null | string;
-	preliminary_results: null | string;
-	requires_clinical_trials: boolean;
-	research_tasks: CreateResearchTaskRequestBody[];
-	risks_and_alternatives: null | string;
-	title: string;
+export interface CreateOrganizationRequestBody {
+	abbreviation?: null | string;
+	full_name: string;
 }
 
-export interface CreateResearchTaskRequestBody {
-	description: null | string;
-	task_number: number;
-	title: string;
-}
-
+// Workspace Types
 export interface CreateWorkspaceRequestBody {
 	description?: null | string;
 	logo_url?: null | string;
 	name: string;
 }
 
-export interface GrantCfp {
-	allow_clinical_trials: boolean;
-	allow_resubmissions: boolean;
-	category: null | string;
-	code: string;
-	description: null | string;
-	funding_organization_id: string;
-	funding_organization_name: string;
-	id: string;
+// Organization Types
+export interface FundingOrganization extends TableIdResponse {
+	abbreviation: null | string;
+	full_name: string;
+}
+
+export interface FundingOrganizationBase extends TableIdResponse {
+	abbreviation: null | string;
+	full_name: string;
+}
+
+export interface GrantApplication extends TableIdResponse {
+	completed_at: null | string;
+	details: ApplicationDetails | null;
+	grant_template: GrantTemplate | null;
+	research_objectives: null | ResearchObjective[];
+	text_generation_results: null | TextGenerationResult[];
 	title: string;
-	url: null | string;
+	workspace_id: string;
+}
+
+export interface GrantApplicationFile {
+	grant_application_id: string;
+	rag_file: RagFileResponse;
+}
+
+export interface GrantTemplate extends TableIdResponse {
+	funding_organization: FundingOrganizationBase | null;
+	grant_sections: GrantSection[];
+	name: string;
+	template: string;
 }
 
 export interface LoginRequestBody {
@@ -91,49 +100,57 @@ export interface LoginResponse {
 	jwt_token: string;
 }
 
+export interface OrganizationFile {
+	funding_organization_id: string;
+	rag_file: RagFileResponse;
+}
+
+// Auth Types
 export interface OTPResponse {
 	otp: string;
 }
 
-export interface ResearchAim {
-	aim_number: number;
-	description: null | string;
-	id: string;
-	preliminary_results: null | string;
-	requires_clinical_trials: boolean;
+// File Types
+export interface RagFileResponse extends TableIdResponse {
+	filename: string;
+	indexing_status: FileIndexingStatusEnum;
+	mime_type: string;
+	size: number;
+}
+
+export interface ResearchObjective {
+	description?: string;
+	number: number;
+	relationships?: string[];
 	research_tasks: ResearchTask[];
-	risks_and_alternatives: null | string;
 	title: string;
 }
 
+// Application Types
 export interface ResearchTask {
-	description: null | string;
-	id: string;
-	task_number: number;
+	description?: string;
+	number: number;
+	relationships?: string[];
 	title: string;
+}
+
+export interface TableIdResponse {
+	id: string;
+}
+
+export interface TextGenerationResult {
+	content: string;
+	type: string;
 }
 
 export interface UpdateApplicationRequestBody {
-	cfp_id?: string;
-	innovation?: null | string;
-	research_aims?: CreateResearchAimRequestBody[];
-	significance?: null | string;
+	research_objectives?: ResearchObjective[];
 	title?: string;
 }
 
-export interface UpdateResearchAimRequestBody {
-	aim_number?: number;
-	description?: null | string;
-	preliminary_results?: null | string;
-	requires_clinical_trials?: boolean;
-	risks_and_alternatives?: null | string;
-	title?: string;
-}
-
-export interface UpdateResearchTaskRequestBody {
-	description?: null | string;
-	task_number?: number;
-	title?: string;
+export interface UpdateOrganizationRequestBody {
+	abbreviation?: null | string;
+	full_name?: string;
 }
 
 export interface UpdateWorkspaceRequestBody {
@@ -142,17 +159,13 @@ export interface UpdateWorkspaceRequestBody {
 	name?: string;
 }
 
-export interface Workspace extends WorkspaceBase {
-	applications: ApplicationBase[];
+export interface Workspace extends WorkspaceBaseResponse {
+	grant_applications: GrantApplication[];
 }
 
-export interface WorkspaceBase extends WorkspaceId {
+export interface WorkspaceBaseResponse extends TableIdResponse {
 	description: null | string;
 	logo_url: null | string;
 	name: string;
-	role: UserRole;
-}
-
-export interface WorkspaceId {
-	id: string;
+	role: UserRoleEnum;
 }
