e79911d91ca4b2f3f16356226b05b9a44d669008
Varun Tandon <varuntandon28121993@gmail.com>
2025-04-02 22:07:06 +0000

    refactor(frontend): add font design tokens, rewrite header ui

--- frontend/src/utils/fonts.ts
+++ frontend/src/utils/fonts.ts
@@ -1,6 +1,27 @@
-import { Inter as FontSans } from "next/font/google";
+import { Cabin, Inter as FontSans, Sora, Source_Sans_3 } from "next/font/google";
 
 export const fontSans = FontSans({
+	display: "swap",
 	subsets: ["latin"],
 	variable: "--font-sans",
+});
+
+export const fontCabin = Cabin({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-heading",
+	weight: ["400", "500", "600"],
+});
+
+export const fontSora = Sora({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-button",
+	weight: ["100", "200", "300", "400"],
+});
+
+export const fontSourceSans = Source_Sans_3({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-body",
 });
