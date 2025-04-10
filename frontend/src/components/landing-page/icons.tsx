f80dd44f2b41cf98b5efab4f22a3e327bb0e7cca
Varun Tandon <varuntandon28121993@gmail.com>
2025-04-10 15:42:44 +0000

    style: change CTAs per requirements

--- frontend/src/components/landing-page/icons.tsx
+++ frontend/src/components/landing-page/icons.tsx
@@ -1,5 +1,35 @@
 import { HTMLProps } from "react";
 
+export function IconBenefitFirst({ height = 15, width = 15, ...props }: HTMLProps<SVGSVGElement>) {
+	return (
+		<svg
+			fill="currentColor"
+			height={height}
+			viewBox="0 -960 960 960"
+			width={width}
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
+		</svg>
+	);
+}
+
+export function IconBenefitSecond({ height = 15, width = 15, ...props }: HTMLProps<SVGSVGElement>) {
+	return (
+		<svg
+			fill="currentColor"
+			height={height}
+			viewBox="0 -960 960 960"
+			width={width}
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path d="M0-240v-63q0-43 44-70t116-27q13 0 25 .5t23 2.5q-14 21-21 44t-7 48v65H0Zm240 0v-65q0-32 17.5-58.5T307-410q32-20 76.5-30t96.5-10q53 0 97.5 10t76.5 30q32 20 49 46.5t17 58.5v65H240Zm540 0v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5q72 0 116 26.5t44 70.5v63H780Zm-455-80h311q-10-20-55.5-35T480-370q-55 0-100.5 15T325-320ZM160-440q-33 0-56.5-23.5T80-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T160-440Zm640 0q-33 0-56.5-23.5T720-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T800-440Zm-320-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-80q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560Zm1 240Zm-1-280Z" />
+		</svg>
+	);
+}
+
 export function IconCalendar({ height = 15, width = 15, ...props }: HTMLProps<SVGSVGElement>) {
 	return (
 		<svg
