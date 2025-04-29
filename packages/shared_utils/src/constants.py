dde66a27360b33e642680753d5496f2f8b8d16d5
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-04-29 17:44:49 +0200

    chore: add tests

---
+++ packages/shared_utils/src/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+CONTENT_TYPE_JSON: Final[str] = "application/json"
+CONTENT_TYPE_TEXT: Final[str] = "text/plain"
+ONE_MINUTE_SECONDS: Final[int] = 60
+MIN_WORDS_RATIO: Final[float] = 0.8
