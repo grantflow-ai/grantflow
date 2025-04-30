3f0a8a2c13dcccd2f89af01b6e34456b5320937d
Varun Tandon <varuntandon28121993@gmail.com>
2025-04-30 20:05:49 +0000

    feat: animate landing page

---
+++ frontend/src/components/landing-page/scroll-fade-element.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
+
+export const ScrollFadeElement: React.FC<{
+	children: ReactNode;
+	className?: string;
+	delay?: number;
+	threshold?: number;
+}> = ({ children, className = "", delay = 0, threshold = 0.1 }) => {
+	const elementRef = React.useRef<HTMLDivElement>(null);
+
+	const { scrollYProgress } = useScroll({
+		offset: ["start end", "end start"],
+		target: elementRef,
+	});
+
+	const opacity = useTransform(scrollYProgress, [0, threshold, threshold + 0.1, 0.75, 1], [0, 0, 0.8, 1, 0]);
+	const y = useTransform(scrollYProgress, [0, threshold, threshold + 0.1, 0.75, 1], [20, 10, 0, 0, -20]);
+
+	return (
+		<motion.div
+			className={className}
+			ref={elementRef}
+			style={{
+				opacity,
+				y,
+			}}
+			transition={{
+				delay,
+				duration: 0.5,
+				ease: "easeInOut",
+			}}
+		>
+			{children}
+		</motion.div>
+	);
+};
