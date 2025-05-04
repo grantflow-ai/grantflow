77fbb4fc8d5dcc8c11a158a919b60f688d3a1987
Varun Tandon <varuntandon28121993@gmail.com>
2025-05-04 18:02:19 +0000

    fix(frontend): share header-footer, setup navigation, fix link refs - to pages

---
+++ frontend/src/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicyPage() {
+	return (
+		<div className="container mx-auto py-10">
+			<h1>Privacy Policy</h1>
+		</div>
+	);
+}
---
+++ frontend/src/app/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function TermsPage() {
+	return (
+		<div className="container mx-auto py-10">
+			<h1>Terms</h1>
+		</div>
+	);
+}
