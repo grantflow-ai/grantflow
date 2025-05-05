9d230c3c9e3c095684a0d4a86f8d853009443cf2
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-05 09:47:36 +0200

    chore: removed storybook testing

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-	addons: ["@storybook/addon-essentials", "@storybook/addon-onboarding", "@storybook/experimental-addon-test"],
+	addons: ["@storybook/addon-essentials", "@storybook/addon-onboarding"],
 	env: (config) => {
 		const envVars = { ...config };
 
@@ -20,7 +20,6 @@ const config: StorybookConfig = {
 	},
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	viteFinal: (config) => {
-		// Use vite-tsconfig-paths to resolve imports according to tsconfig paths
 		config.plugins = [...(config.plugins ?? []), tsconfigPaths()];
 
 		return config;
