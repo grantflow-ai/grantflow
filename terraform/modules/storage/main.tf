c494a3f091b5bc0c63990828bbf9028d19f04d39
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-17 15:11:41 +0200

    chore: applied linting

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,5 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+variable "bucket_name" {
+  description = "The name of the storage bucket"
+  type        = string
+  default     = "grantflow-uploads"
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
 resource "google_storage_bucket" "uploads" {
-  name                        = "grantflow-uploads"
+  name                        = var.bucket_name
   location                    = "US"
   force_destroy               = false
   storage_class               = "STANDARD"
@@ -8,6 +30,28 @@ resource "google_storage_bucket" "uploads" {
 
   soft_delete_policy {
     retention_duration_seconds = 604800
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.bucket_key.id
+  }
+
+  labels = {
+    environment = var.environment
+  }
+}
+
+resource "google_kms_key_ring" "storage_keyring" {
+  name     = "storage-keyring"
+  location = "us"
+}
+
+resource "google_kms_crypto_key" "bucket_key" {
+  name     = "storage-bucket-key"
+  key_ring = google_kms_key_ring.storage_keyring.id
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
