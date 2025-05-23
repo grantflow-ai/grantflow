f1c6c8bad895abf4707e10cd803490868f4187cb
Varun Tandon <varuntandon28121993@gmail.com>
2025-05-23 13:37:55 +0000

    feat: application wizard wrapper component with functional elements

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -13,4 +13,35 @@ function IconGoAhead({ height = 15, width = 15, ...props }: React.HTMLProps<SVGS
 	);
 }
 
-export { IconGoAhead };
+function IconGoBack({ height = 15, width = 15, ...props }: React.HTMLProps<SVGSVGElement>) {
+	return (
+		<svg
+			fill="currentColor"
+			height={height}
+			viewBox="0 0 16 16"
+			width={width}
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<mask
+				height="16"
+				id="mask0_1175_4938"
+				maskUnits="userSpaceOnUse"
+				style={{ maskType: "alpha" }}
+				width="16"
+				x="0"
+				y="0"
+			>
+				<rect fill="currentColor" height="16" width="16" />
+			</mask>
+			<g mask="url(#mask0_1175_4938)">
+				<path
+					d="M9.33334 12L5.33334 8L9.33334 4L10.2667 4.93333L7.20001 8L10.2667 11.0667L9.33334 12Z"
+					fill="currentColor"
+				/>
+			</g>
+		</svg>
+	);
+}
+
+export { IconGoAhead, IconGoBack };
