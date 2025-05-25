55af9a94d10f957dae74948dfb687cb9ca3f41fb
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-25 21:09:03 +0200

    chore: updated crawler to use pubsub

--- terraform/modules/pubsub/outputs.tf
+++ terraform/modules/pubsub/outputs.tf
@@ -17,3 +17,23 @@ output "file_indexing_subscription_name" {
   description = "The name of the file indexing subscription"
   value       = google_pubsub_subscription.file_indexing_subscription.name
 }
+
+output "url_crawling_topic_id" {
+  description = "The ID of the URL crawling topic"
+  value       = google_pubsub_topic.url_crawling.id
+}
+
+output "url_crawling_topic_name" {
+  description = "The name of the URL crawling topic"
+  value       = google_pubsub_topic.url_crawling.name
+}
+
+output "url_crawling_subscription_id" {
+  description = "The ID of the URL crawling subscription"
+  value       = google_pubsub_subscription.url_crawling_subscription.id
+}
+
+output "url_crawling_subscription_name" {
+  description = "The name of the URL crawling subscription"
+  value       = google_pubsub_subscription.url_crawling_subscription.name
+}
