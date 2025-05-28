b71595bb685a4d6407f914b9aa447888b7da0c8b
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-28 21:59:52 +0200

    feat: add segment source
    Signed-off-by: Na'aman Hirschfeld <nhirschfeld@gmail.com>

---
+++ frontend/src/utils/segment.ts
@@ -0,0 +1,27 @@
+import { AnalyticsBrowser } from "@segment/analytics-next";
+
+import { getEnv } from "./env";
+
+const analytics: { value: AnalyticsBrowser | null } = {
+	value: null,
+};
+
+export const getAnalytics = () => {
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	if (!analytics.value && globalThis.window !== undefined) {
+		analytics.value = AnalyticsBrowser.load({ writeKey: getEnv().NEXT_PUBLIC_SEGMENT_WRITE_KEY });
+	}
+
+	return analytics.value;
+};
+
+export async function analyticsIdentify(
+	userId: string,
+	traits: {
+		email: string;
+		firstName: string;
+		lastName: string;
+	},
+) {
+	await getAnalytics()?.identify(userId, traits);
+}
