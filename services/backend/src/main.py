fc467109f2075f1ae05bde8ecbd1ce27c8d4837a
Nader Alexan <alexan.nader@gmail.com>
2025-05-29 20:36:03 +0300

    feat(backend): accept invitation endpoint

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -24,6 +24,7 @@ from services.backend.src.api.routes.sources import (
     handle_retrieve_rag_sources,
 )
 from services.backend.src.api.routes.workspaces import (
+    handle_accept_invitation,
     handle_create_invitation_redirect_url,
     handle_create_workspace,
     handle_delete_invitation,
@@ -61,6 +62,7 @@ api_routes: list[HTTPRouteHandler | WebsocketRouteHandler] = [
     handle_update_workspace,
     handle_delete_invitation,
     handle_update_invitation_role,
+    handle_accept_invitation,
 ]
 
 grant_template_generation_pipeline_handler_listener = listener("grant_template_generation_pipeline_handler")(
