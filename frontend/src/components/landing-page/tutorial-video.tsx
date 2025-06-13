9b1f6bf69275f8da7ec05c2afd526040b670e90f
naderalexan <alexan.nader@gmail.com>
2025-06-13 15:22:07 +0300

    feat(frontend): add different video formats for different devices

---
+++ frontend/src/components/landing-page/tutorial-video.tsx
@@ -0,0 +1,17 @@
+// TutorialVideo.tsx
+"use client";
+
+import React from "react";
+
+export function TutorialVideo() {
+	return (
+		<div className="my-8 flex w-full justify-center bg-white">
+			<video autoPlay className="h-auto max-w-full shadow-lg" controls loop muted playsInline>
+				<source media="(min-width: 1280px)" src="/assets/tutorial-1080p.mp4" type="video/mp4" />
+				<source media="(min-width: 768px)" src="/assets/tutorial-720p.mp4" type="video/mp4" />
+				<source src="/assets/tutorial-480p.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+		</div>
+	);
+}
