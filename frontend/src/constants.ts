2b070d1564dbc83eab87a816c4651f0546ea35cc
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-06-17 19:03:41 +0200

    chore: re-introduced constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,18 @@
+export const ONE_MINUTE_IN_MS = 60 * 1000;
+export const FIVE_SECONDS = 5 * 1000;
+export const TEN_MINUTES = 10 * 60 * 1000;
+export const ONE_HOUR_IN_SECONDS = 60 * 60;
+export const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
+export const FIREBASE_LOCAL_STORAGE_KEY = "firebase-signin-email";
+export const SESSION_COOKIE = "grantflow_session";
+
+export const WIZARD_STEP_TITLES = [
+	"Application Details",
+	"Application Structure",
+	"Knowledge Base",
+	"Research Plan",
+	"Research Deep Dive",
+	"Generate and Complete",
+] as const;
+
+export type WizardStepTitlesType = typeof WIZARD_STEP_TITLES;
