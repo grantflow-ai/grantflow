1d052350adcd889c846d477a85e3473c2e4983c3
v-tan <varuntandon28121993@gmail.com>
2025-06-17 23:26:48 +0530

    chore: fix compilation, tests

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,6 +5,7 @@ export const ONE_HOUR_IN_SECONDS = 60 * 60;
 export const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
 export const FIREBASE_LOCAL_STORAGE_KEY = "firebase-signin-email";
 export const SESSION_COOKIE = "grantflow_session";
+export const FIREBASE_LOCAL_STORAGE_KEY = "firebase-signin-email";
 
 export const WIZARD_STEP_TITLES = [
 	"Application Details",
