396f1af5eb94c4e91f7b2635f18544b9306b4ff8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-06-28 17:14:46 +0200

    chore: remove RAG service files from non-rag branch

--- services/rag/src/utils/prompt_template.py
+++
@@ -1,9 +0,0 @@
-from typing import final
-
-from packages.shared_utils.src.serialization import serialize
-from prompt_template import PromptTemplate as BasePromptTemplate
-
-
-@final
-class PromptTemplate(BasePromptTemplate):
-    serialize = staticmethod(lambda value: serialize(value).decode())
