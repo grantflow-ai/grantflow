1008c17453308f75089d7bb44af310f865cdaa4c
yatanvesh <yatan.vesh@gmail.com>
2025-07-09 19:23:04 +0530

    feat: setup export button, prompt window and editor sections

---
+++ frontend/src/components/projects/applications/editor/editor-container.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { EditorExportButton } from "./editor-export-button";
+import { EditorPromptWindow } from "./editor-prompt-window";
+import { EditorSections } from "./editor-sections";
+import { EditorWarning } from "./editor-warning";
+import { GrantApplicationEditor } from "./grant-application-editor";
+
+export function EditorContainer() {
+	return (
+		<div className="flex flex-col gap-4 pr-6 h-full">
+			<EditorWarning />
+			<div className="flex gap-3 h-full">
+				<div className="min-w-[326px] w-[326px]">
+					<EditorPromptWindow />
+				</div>
+				<GrantApplicationEditor
+					// @ts-expect-error Update the props
+					application={{ text: "hello grantflow editor" }}
+				/>
+				<div className="min-w-[207px] w-[207px] flex flex-col gap-3">
+					<EditorExportButton />
+					<EditorSections />
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ frontend/src/components/projects/applications/editor/editor-warning.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export function EditorWarning() {
+	const [visible, setVisible] = useState(true);
+
+	if (!visible) {
+		return null;
+	}
+
+	return (
+		<div className="flex justify-between items-center bg-light-gray p-2 rounded-sm border-app-slate-blue border">
+			<div className="flex gap-1 items-center">
+				<Image alt="Warning" height={16} src="/icons/info.svg" width={16} />
+				<p className="text-sm text-app-black">
+					Keep in mind that AI has limitations and may occasionally make mistakes. Always review and refine
+					the output.
+				</p>
+			</div>
+			<button
+				className="cursor-pointer"
+				onClick={() => {
+					setVisible(false);
+				}}
+				type="button"
+			>
+				<Image alt="Close" height={16} src="/icons/close.svg" width={16} />
+			</button>
+		</div>
+	);
+}
