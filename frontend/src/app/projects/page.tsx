25f169c76ec17a09d90dd2882d0cd265f0539783
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-07-16 11:24:28 +0100

    commit before rebase

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -1,8 +1,10 @@
+import { ProjectListItemFactory } from "::testing/factories";
 import { getProjects } from "@/actions/project";
 import { DashboardClient } from "@/components/projects/dashboard/dashboard-client";
 
 export default async function DashboardPage() {
 	const initialProjects = await getProjects();
+	const mockProjects = ProjectListItemFactory.batch(3)
 
-	return <DashboardClient initialProjects={initialProjects} />;
+	return <DashboardClient initialProjects={mockProjects} />;
 }
