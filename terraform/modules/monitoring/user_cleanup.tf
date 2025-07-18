625f65de49d46d11115a674d606bd0fbbda77f9f
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-07-19 00:18:00 +0200

    fix: use relative paths without path.module for terraform files
    - Remove path.module prefix as it complicates path resolution
    - Use simple relative paths from environments/staging directory
    - Files are at ../../../cloud_functions relative to staging dir

--- terraform/modules/monitoring/user_cleanup.tf
+++ terraform/modules/monitoring/user_cleanup.tf
@@ -31,12 +31,12 @@ data "archive_file" "entity_cleanup_source" {
   output_path = "${path.module}/entity-cleanup-function.zip"
 
   source {
-    content  = file("${path.module}/../../../cloud_functions/src/user_cleanup/main.py")
+    content  = file("../../../cloud_functions/src/user_cleanup/main.py")
     filename = "main.py"
   }
 
   source {
-    content  = file("${path.module}/../../../cloud_functions/requirements.txt")
+    content  = file("../../../cloud_functions/requirements.txt")
     filename = "requirements.txt"
   }
 }
