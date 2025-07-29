d507ed5906d873d447d66ca74a681b09e231cb56
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-07-29 17:09:37 +0100

    lint correction

--- frontend/src/components/layout/app-header.tsx
+++ frontend/src/components/layout/app-header.tsx
@@ -15,23 +15,23 @@ interface AppHeaderProps {
 
 export function AppHeader({ projectTeamMembers }: AppHeaderProps) {
 	const [isNotificationOpen, setNotificationOpen] = useState(false);
-	const notificationRef = useRef<HTMLDivElement>(null);
+	const notificationRef = useRef<HTMLButtonElement>(null);
 	useOnClickOutside(notificationRef, () => {
 		setNotificationOpen(false);
 	});
 	return (
 		<header className="h-[73px] w-full flex justify-end items-center gap-1 px-6" data-testid="dashboard-header">
-			<div
+			<button
 				className="size-8 flex items-center justify-center rounded-sm hover:bg-app-gray-100/50 p-2"
 				data-testid="dashboard-notification"
 				onClick={() => {
 					setNotificationOpen(!isNotificationOpen);
 				}}
 				ref={notificationRef}
-				role="button"
+				type="button"
 			>
 				<Notification isOpen={isNotificationOpen} />
-			</div>
+			</button>
 			<div className="flex items-center" data-testid="dashboard-avatar-group">
 				<AvatarGroup className="border-0 rounded-none" size="md" users={projectTeamMembers} />
 			</div>
