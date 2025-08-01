4177fba674e6d8e3ee709f254f7ca231213cdbf0
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-08-01 20:57:34 +0100

    fix:test

--- frontend/src/components/organizations/project/project-detail-client.tsx
+++ frontend/src/components/organizations/project/project-detail-client.tsx
@@ -36,7 +36,7 @@ export function ProjectDetailClient() {
 	const { selectedOrganizationId } = useOrganizationStore();
 	const { navigateToApplication } = useNavigationStore();
 	const { user } = useUserStore();
-	const { closeModal, isModalOpen, openModal } = useNewApplicationModalStore();
+	const { closeModal, isModalOpen } = useNewApplicationModalStore();
 	const { getProjects, projects } = useProjectStore();
 
 	const [searchQuery, setSearchQuery] = useState("");
@@ -260,6 +260,7 @@ export function ProjectDetailClient() {
 									aria-label="Project Title"
 									className="font-medium text-[36px] leading-[42px] text-gray-300 outline-none border-b-2  border-primary focus:ring-offset-2  "
 									contentEditable={true}
+									data-testid="project-title-input"
 									onBlur={handleSaveProjectTitle}
 									onKeyDown={(e) => {
 										if (e.key === "Enter") {
@@ -341,7 +342,7 @@ export function ProjectDetailClient() {
 								className="px-4 py-2"
 								data-testid="new-application-button"
 								disabled={isCreatingApplication}
-								onClick={() => {openModal()}}
+								onClick={handleCreateApplication}
 								type="button"
 								variant="primary"
 							>
