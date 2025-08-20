40ce6a8f36a46b60b3664860e2371b4d1b6fd1cf
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-08-20 20:03:10 +0200

    chore: fixed devdependecy

--- frontend/src/components/email-templates/index.ts
+++ frontend/src/components/email-templates/index.ts
@@ -4,8 +4,6 @@ import OrganizationDeletedTemplate from "./organization-deleted-template";
 import PersonalAccountDeletionTemplate from "./personal-account-deletion-template";
 import WelcomeEmailTemplate from "./welcome-email-template";
 
-// The react-email preview server expects a default export
-// with the templates as properties.
 export default {
 	EmailVerificationTemplate,
 	InvitationEmailTemplate,
@@ -13,11 +11,3 @@ export default {
 	PersonalAccountDeletionTemplate,
 	WelcomeEmailTemplate,
 };
-
-// We can keep the named exports for the rest of the app.
-
-export { default as EmailVerificationTemplate } from "./email-verification-template";
-export { default as InvitationEmailTemplate } from "./invitation-email-template";
-export { default as OrganizationDeletedTemplate } from "./organization-deleted-template";
-export { default as PersonalAccountDeletionTemplate } from "./personal-account-deletion-template";
-export { default as WelcomeEmailTemplate } from "./welcome-email-template";
