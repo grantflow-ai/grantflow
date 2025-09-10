7b654170049e017989ad1a3180f8cf26cee52af1
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-09-10 12:14:46 +0200

    chore: updated email webhook to retrieve firebase users

--- terraform/modules/pubsub/main.tf
+++ terraform/modules/pubsub/main.tf
@@ -321,13 +321,9 @@ resource "google_pubsub_subscription" "email_notifications_subscription" {
   push_config {
     push_endpoint = "${var.backend_url}/webhooks/pubsub/email-notifications"
 
-    oidc_token {
-      service_account_email = var.pubsub_invoker_service_account_email
-      audience              = var.backend_url
-    }
-
     attributes = {
       "x-goog-version" = "v1"
+      "Authorization"  = var.pubsub_webhook_token
     }
   }
 
