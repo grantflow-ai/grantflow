91209bf306cd98220e6579bff2d2ee87fd91b3f4
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-09-19 12:41:41 +0100

    chore: clean up

--- frontend/src/components/organizations/project/applications/wizard/ai-autofill-button..tsx
+++ frontend/src/components/organizations/project/applications/wizard/ai-autofill-button..tsx
@@ -16,12 +16,12 @@ export function AiAutofillButton({ className, disabled, isLoading, onClick }: Ai
 			className={cn(
 				"group items-center justify-center rounded-[40px] flex p-[1px] bg-[linear-gradient(to_right,#7068FF_0%,#433F99_99%)]",
 				className,
-				(disabled || isLoading) && "cursor-not-allowed opacity-50",
+				(disabled ?? isLoading) && "cursor-not-allowed opacity-50",
 			)}
 		>
 			<button
 				className="relative items-center rounded-[40px] flex gap-1 px-4 py-1 bg-[linear-gradient(to_left,#1E13F8_2%,#392FFA_20%,#4B42FC_42%,#635BFEB5_99%)] text-white shiny overflow-hidden transition-transform active:scale-95 cursor-pointer"
-				disabled={disabled || isLoading}
+				disabled={disabled ?? isLoading}
 				onClick={onClick}
 				type="button"
 			>
